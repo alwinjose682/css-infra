db6c8e87630539cabac7f6329974aba61c2774d7
alwinjose682 <alwinjoseth@gmail.com>
2025-06-10 20:54:16 +0200

    set maxvalue for DB sequences

--- oracle-db/install__1_0/sequences/cashflow_rejection_seq.sql
+++ oracle-db/install__1_0/sequences/cashflow_rejection_seq.sql
@@ -1,4 +1,6 @@
 create sequence cashflow_rejection_seq
+MINVALUE 1
+MAXVALUE 9223372036854775807
 start with 1
 increment by 1
 cache 10
--- oracle-db/install__1_0/sequences/cashflow_seq.sql
+++ oracle-db/install__1_0/sequences/cashflow_seq.sql
@@ -1,4 +1,6 @@
 create sequence cashflow_seq
+MINVALUE 1
+MAXVALUE 9223372036854775807
 start with 1
 increment by 1
 cache 1000
